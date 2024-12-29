ddce04ffef65f3c0a30eb3179f590e7431e32776
mihail <mihail.bashkatov@gmail.com>
2024-12-29 14:51:44 +0100

    admin panel created

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+# Registering admin panel for Product
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name',
+                    'product_description',
+                    'product_image',
+                    'category',
+                    'product_price',
+                    'created_at',
+                    'updated_at'
+                    )
+
+    list_filter = ('product_name',
+                    'product_description',
+                    'product_image',
+                    'category',
+                    'product_price',
+                    'created_at',
+                    'updated_at'
+                   )
+
+    search_fields = ('product_name',
+                    'product_description',
+                    'category',
+                    )
+
+
+# Registering admin panel for Category
+@admin.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('category_name',
+                    'category_description',
+                    )
+
+    list_filter = ('category_name',
+                   )
+
+    search_fields = ('category_name',
+                    'category_description',
+                    )
